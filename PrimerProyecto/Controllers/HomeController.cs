21c6c52f2fef54476efe130312a13034ca01a77e
Ben wis <benwis678@gmail.com>
2024-07-02 23:04:27 -0300

    Programación defensiva ¿y mucho más?

--- PrimerProyecto/Controllers/HomeController.cs
+++ PrimerProyecto/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using System.Timers;
+using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Mvc;
 using PrimerProyecto.Models;
 
@@ -19,26 +20,58 @@ public class HomeController : Controller
         ViewBag.Paquetes = ORTWorld.Paquetes;
         return View();
     }
-    public IActionResult SelectPaquete ()
+    public IActionResult SelectPaquete (string? error)
     {
         ViewBag.ListaAereos = ORTWorld.ListaAereos;
         ViewBag.ListaDestinos = ORTWorld.ListaDestinos;
         ViewBag.ListaExcursiones = ORTWorld.ListaExcursiones;
         ViewBag.ListaHoteles = ORTWorld.ListaHoteles;
+        if (error != null) {
+            ViewBag.MensajeError = error;
+        }
         return View();
     }
+
+    private static bool ValidarPaquete(int Destino, int Hotel, int Aereo, int Excursion)
+    {
+        if (Destino > ORTWorld.ListaDestinos.Count || Destino < 1)
+        {
+            return false;
+        }
+        if (Hotel > ORTWorld.ListaHoteles.Count || Hotel < 1)
+        {
+            return false;
+        }
+        if (Aereo > ORTWorld.ListaAereos.Count || Hotel < 1)
+        {
+            return false;
+        }
+        if (Excursion > ORTWorld.ListaExcursiones.Count || Hotel < 1)
+        {
+            return false;
+        }
+        return true;
+    }
+
     public IActionResult GuardarPaquete (int Destino, int Hotel, int Aereo, int Excursion)
     {
         bool exito;
-        Paquete NuevoPaquete = new Paquete(ORTWorld.ListaHoteles [Hotel-1], ORTWorld.ListaAereos[Aereo-1], ORTWorld.ListaExcursiones[Excursion-1]);
-        exito = ORTWorld.IngresarPaquete(ORTWorld.ListaDestinos[Destino-1], NuevoPaquete);
-        if(exito)
+        if (ValidarPaquete(Destino,Hotel,Aereo,Excursion))
         {
-            ViewBag.creacionPaquete = "Se logro crear el paquete";
+            Paquete NuevoPaquete = new Paquete(ORTWorld.ListaHoteles [Hotel-1], ORTWorld.ListaAereos[Aereo-1], ORTWorld.ListaExcursiones[Excursion-1]);
+            exito = ORTWorld.IngresarPaquete(ORTWorld.ListaDestinos[Destino-1], NuevoPaquete);
+            if(exito)
+            {
+                ViewBag.creacionPaquete = "Se logro crear el paquete";
+            }
+            else{
+                ViewBag.creacionPaquete = "No se logro crear el paquete";
+            }
+            return RedirectToAction("Index");
         }
-        else{
-            ViewBag.creacionPaquete = "No se logro crear el paquete";
+        else
+        {
+            return RedirectToAction("SelectPaquete", new { error = "Error, ingrese nuevamente" });
         }
-        return RedirectToAction("Index");
     }
 }
