04d0d77837089b401d9fd1c1497374221aee2c7b
BEN WISCHÑEVSKY <48591419@ort>
2024-07-03 10:17:12 -0300

    cambios css

--- PrimerProyecto/Models/ORTWorld.cs
+++ PrimerProyecto/Models/ORTWorld.cs
@@ -1,10 +1,10 @@
 public static class ORTWorld 
 {
-    public static List<string> ListaDestinos {get; private set;} = ["Roma", "Barcelona", "Madrid", "CABA", "Venecia", "Tokio", "Hong Kong", "Moscu", "Brujas", "New York"];
-    public static List<string> ListaHoteles {get; private set;} = ["Sheraton", "Hilton", "The Peninsula", "Waldorf-Astoria", "Savoy"];
-    public static List<string> ListaAereos {get; private set;} = ["aereo1.png", "aereo2.png", "aereo3.png", "aereo4.png", "aereo5.png"];
-    public static List<string> ListaExcursiones {get; private set;} = ["excursion1.jpg", "excursion2.jpg", "excursion3.jpg", "excursion4.jpg", "excursion5.jpg"];
-    public static Dictionary<string, Paquete> Paquetes {get; private set;} = [];
+    public static List<string> ListaDestinos {get; private set;} = new List<string>  {"Roma", "Barcelona", "Madrid", "CABA", "Venecia", "Tokio", "Hong Kong", "Moscu", "Brujas", "New York"};
+    public static List<string> ListaHoteles {get; private set;} = new List<string> {"Sheraton", "Hilton", "The Peninsula", "Waldorf-Astoria", "Savoy"};
+    public static List<string> ListaAereos {get; private set;} = new List<string> {"aereo1.png", "aereo2.png", "aereo3.png", "aereo4.png", "aereo5.png"};
+    public static List<string> ListaExcursiones {get; private set;} = new List<string> {"excursion1.jpg", "excursion2.jpg", "excursion3.jpg", "excursion4.jpg", "excursion5.jpg"};
+    public static Dictionary<string, Paquete> Paquetes {get; private set;} = new Dictionary<string, Paquete>();
 
 public static bool IngresarPaquete(string destinoSeleccionado, Paquete paquete)
 {
